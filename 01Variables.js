b222b2ad362e0b97eac4b5b82031b102b48bf04d
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-23 23:06:41 +0530

    Variables

---
+++ 01Variables.js
@@ -0,0 +1,13 @@
+var fullName = "Shubham Pandey";
+
+var courseName = "Code2gether JAVA";
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+console.log(loggedCount);
+console.log("loggedCount");
+
+var paymentMode;
+paymentMode = "Credir Card";
+console.log(paymentMode);
