da76b5046421e4fa5961fd1785c58910b1b8519c
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-23 23:07:29 +0530

    conditionals

---
+++ 04Conditionals.js
@@ -0,0 +1,18 @@
+var temperature;
+
+//TODO: Go to google and get the data
+
+temperature = 20;
+
+// var result = temperature < 20;
+// console.log(result);
+
+if(temperature < 20){
+    console.log("It is very cold outside");
+}
+
+if (temperature < 30) {
+    console.log("It's Moderate outside")
+} else {
+    console.log("It's very Hot outside")
+}
