c47dabb1afe873e6695a77350c02e56d06cfb85d
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-26 23:56:02 +0530

    FirstAssignment

---
+++ firstAssignment.js
@@ -0,0 +1,21 @@
+const uid = "123@abc";
+
+var firstName = "Shubham";
+var lastName = "Pandey";
+var email = "Shubham@pandey.com";
+var country = "India";
+var state = "Delhi";
+var password = "12345678";
+var confirmPassword = "12345678";
+var isLoggedInFromGoogle = false;
+var isLoggedInFromFacebook = false;
+
+console.log(`
+  With unique ID : ${uid}
+  first name is : ${firstName}
+  last name is : ${lastName}
+  and Email id is : ${email}
+  Belongs to : ${country}
+  lives in : ${state}
+  his password is : ${password}
+`);
