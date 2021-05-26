35c8be76a772973027a628f55e0b71a084c66a1b
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-26 23:54:25 +0530

    login

---
+++ 05_logIn.js
@@ -0,0 +1,20 @@
+var isEmail = true;
+var isGoogle = false;
+var isFacebook = true;
+
+// if(isEmail){
+//     console.log("You are Logged in");
+// } 
+
+// if (isGoogle) {
+//     console.log("You are Logged in");
+// }
+
+// if (isFacebook) {
+//     console.log("You are Logged in");
+// }
+
+
+if(isEmail || isFacebook || isGoogle){
+    console.log("You are Logged in and check our course contents");
+}
