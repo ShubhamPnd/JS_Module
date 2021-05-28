446030a8c1433f15031eb55a7a3e5ef75995c4dc
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-28 23:16:55 +0530

    slicing_splice

---
+++ Intermediate/10_Slicing_Splice.js
@@ -0,0 +1,10 @@
+var users = ["Tim" ," tom" , "jake", "sam","raghu","preet"];
+
+// console.log(users.slice(1,3));
+// console.log(users.slice(1,4));
+// console.log(users.slice(1));
+
+// Splice Mehod
+
+users.splice(1,3,"Shubh"); // This method removes the value and add the given string
+console.log(users);
