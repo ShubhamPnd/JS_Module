f140d3f73e98e6593d68980c9456d09780b4fd2b
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-29 23:43:55 +0530

    while

---
+++ Intermediate/14_whileLoop.js
@@ -0,0 +1,17 @@
+const myStates = ["Rajasthan",
+"Delhi",
+"Haryana" ,
+"Punjab",
+1998,
+"Uttar Pradesh",
+"Uttrakhand"];
+
+let i = 0;
+while (i < myStates.length) {
+    //   if (typeof myStates[i] !== "string") {
+    //     continue;
+    // }
+    console.log(myStates[i]);
+    i++;
+  
+}
