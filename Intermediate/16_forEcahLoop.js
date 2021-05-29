4156d7846793d74dc12693486ed535bd02fd3631
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-05-29 23:45:57 +0530

    foreach

---
+++ Intermediate/16_forEcahLoop.js
@@ -0,0 +1,18 @@
+// For each loop is not technically considered as loop because it comes under method .
+const myStates = ["Rajasthan",
+"Delhi",
+"Haryana" ,
+"Punjab",
+1998,
+"Uttar Pradesh",
+"Uttrakhand"];
+
+// var i =0 ;
+// for (;;) {
+//     if(i < 3)
+//     console.log(i);
+//     i++
+// }
+
+myStates.forEach((s) => (console.log(s)));
+myStates.forEach((s) => console.log(s));
